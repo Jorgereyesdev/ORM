f081cc31129999f7ae64b6432a898a8607f10061
Jorgereyesdev <jorgereyes2017@gmail.com>
2024-08-23 13:10:24 -0500

    first commit

---
+++ src/main/java/com/ormproyect/demo/OrmProyectApplication.java
@@ -0,0 +1,13 @@
+package com.ormproyect.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmProyectApplication.class, args);
+	}
+
+}
